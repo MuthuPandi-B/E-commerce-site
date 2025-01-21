1cf625e72b713ea849cd7398d8574f83538e8c42
MuthuPandi-B <muthup61@gmail.com>
2025-01-21 20:36:42 +0530

    logo added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ({ cartCount }) => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex justify-between items-center p-4 ">
-      <div className="flex gap-8"> < img className="h-10 w-12 rounded-full" src="" alt="Logo Image" />
+      <div className="flex gap-8"> < img className="h-10 w-12 rounded-full" src={logo} alt="Logo Image" />
       <h1 className="text-2xl font-semibold  text-gray-900 dark:text-white">One Click Shopping</h1></div>
         <ul className="flex space-x-4">
           <li>
